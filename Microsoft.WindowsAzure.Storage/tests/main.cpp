1ab5a0380910f4d3d9d91073940f41b82f7f6154
Jinming Hu <jinmhu@microsoft.com>
2019-09-04 15:04:34 +0800

    Fix some infrequent test case failures.

--- Microsoft.WindowsAzure.Storage/tests/main.cpp
+++ Microsoft.WindowsAzure.Storage/tests/main.cpp
@@ -17,6 +17,8 @@
 
 #include "stdafx.h"
 
+#include <unordered_set>
+
 #include "was/blob.h"
 
 #ifndef _WIN32
@@ -30,17 +32,26 @@
 #endif
 
 
-int run_tests(const char* suite_name, const char* test_name)
+int run_tests(const std::unordered_set<std::string>& included_cases, const std::unordered_set<std::string>& excluded_cases)
 {
     UnitTest::TestReporterStdout reporter;
     UnitTest::TestRunner runner(reporter);
-    return runner.RunTestsIf(UnitTest::Test::GetTestList(), suite_name, [test_name] (UnitTest::Test* test) -> bool
+
+    auto match = [](const std::unordered_set<std::string>& all_cases, const std::string& suite_name, const std::string& test_name)
     {
-        return (test_name == NULL) || (!strcmp(test_name, test->m_details.testName));
+        return all_cases.find(suite_name) != all_cases.end() || all_cases.find(suite_name + ":" + test_name) != all_cases.end();
+    };
+
+    return runner.RunTestsIf(UnitTest::Test::GetTestList(), nullptr, [match, included_cases, excluded_cases](const UnitTest::Test* test) -> bool
+    {
+        std::string suite_name = test->m_details.suiteName;
+        std::string test_name = test->m_details.testName;
+
+        return !match(excluded_cases, suite_name, test_name) && (included_cases.empty() || match(included_cases, suite_name, test_name));
     }, 0);
 }
 
-int main(int argc, const char* argv[])
+int main(int argc, const char** argv)
 {
     azure::storage::operation_context::set_default_log_level(azure::storage::client_log_level::log_level_verbose);
 
@@ -58,30 +69,39 @@ int main(int argc, const char* argv[])
 
 #endif
 
-    int failure_count;
-    if (argc == 1)
+    std::unordered_set<std::string> included_cases;
+    std::unordered_set<std::string> excluded_cases;
+
+    auto starts_with = [](const std::string& str, const std::string& prefix)
     {
-        failure_count = run_tests(NULL, NULL);
-    }
-    else
+        size_t i = 0;
+        while (i < str.length() && i < prefix.length() && prefix[i] == str[i]) ++i;
+        return i == prefix.length();
+    };
+
+    auto add_to = [](std::unordered_set<std::string>& all_cases, const std::string& name)
     {
-        failure_count = 0;
-        for (int i = 1; i < argc; ++i)
+        auto colon_pos = name.find(":");
+        std::string suite_name = name.substr(0, colon_pos);
+        if (suite_name.empty())
         {
-            std::string arg(argv[i]);
-            auto colon = arg.find(':');
-            if (colon == std::string::npos)
-            {
-                failure_count += run_tests(argv[i], NULL);
-            }
-            else
-            {
-                auto suite_name = arg.substr(0, colon);
-                auto test_name = arg.substr(colon + 1);
-                failure_count += run_tests(suite_name.c_str(), test_name.c_str());
-            }
+            throw std::invalid_argument("Invalid test case \"" + name + "\".");
+        }
+        all_cases.emplace(name);
+    };
+
+    for (int i = 1; i < argc; ++i)
+    {
+        std::string arg(argv[i]);
+        if (starts_with(arg, "--exclude="))
+        {
+            add_to(excluded_cases, arg.substr(arg.find('=') + 1));
+        }
+        else
+        {
+            add_to(included_cases, arg);
         }
     }
 
-    return failure_count;
+    return run_tests(included_cases, excluded_cases);
 }
